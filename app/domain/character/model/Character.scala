8233460f1e89b1f1ffd6674ed41c913b77cd50c9
Taketo Ikeda <t_ikeda@septeni-original.co.jp>
2016-08-21 21:16:42 +0900

    characterのモデルを作成

---
+++ app/domain/character/model/Character.scala
@@ -0,0 +1,25 @@
+package domain.character.model
+
+class Character(
+                 val id: Long,
+                 val name: String,
+                 val fullName: String,
+                 val realName: String,
+                 val role: Role,
+                 val skills: String,
+                 val imageUrl: String
+               )
+
+object Character {
+  def apply(id: Long, name: String, fullName: String, realName: String, role: Role, skills: String, imageUrl: String) =
+    new Character(
+      id = id,
+      name = name,
+      fullName = fullName,
+      realName = realName,
+      role = role,
+      skills = skills,
+      imageUrl = imageUrl
+    )
+}
+
---
+++ app/domain/character/model/Role.scala
@@ -0,0 +1,24 @@
+package domain.character.model
+
+sealed abstract class Role(val value: Int )
+
+object Role {
+
+  case object Programmer extends Role(1)
+  case object BackOffice  extends Role(2)
+  case object BoardMember extends Role(3)
+  case object President extends Role(4)
+  case object Incubator extends Role(5)
+  case object Assistant extends Role(6)
+
+  def valueToObject(v:Int): Role = {
+   v match {
+     case 1 => Programmer
+     case 2 => BackOffice
+     case 3 => BoardMember
+     case 4 => President
+     case 5 => Incubator
+     case 6 => Assistant
+   }
+  }
+}
