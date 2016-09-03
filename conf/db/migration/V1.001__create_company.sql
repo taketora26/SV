e120c7ff6c1ccb629c0080e41f3f2e6a7f0c018a
Taketo Ikeda <t_ikeda@septeni-original.co.jp>
2016-09-04 03:19:28 +0900

    マイグレーション修正

--- conf/db/migration/V1.001__create_company.sql
+++ conf/db/migration/V1.001__create_company.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `company` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(256) DEFAULT NULL,
-  `product_id` bigint(20) unsigned NOT NULL,
+  `company_product_id` bigint(20) unsigned NOT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
-INSERT INTO `company` (`id`, `name`, `product_id`)
+INSERT INTO `company` (`id`, `name`, `company_product_id`)
 VALUES
 (1,'PiedPiper',1),
 (2,'Holli',2);
