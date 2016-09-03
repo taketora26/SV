88baa63d5c01b07b6191d204d8986ba17c9d5d1a
Taketo Ikeda <t_ikeda@septeni-original.co.jp>
2016-09-04 03:00:16 +0900

    CompanyServiceをCompanyProductに変更

--- conf/db/migration/V1.001__create_company.sql
+++ conf/db/migration/V1.001__create_company.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `company` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(256) DEFAULT NULL,
-  `service_id` bigint(20) unsigned NOT NULL,
+  `product_id` bigint(20) unsigned NOT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
-INSERT INTO `company` (`id`, `name`, `service_id`)
+INSERT INTO `company` (`id`, `name`, `product_id`)
 VALUES
 (1,'PiedPiper',1),
 (2,'Holli',2);
--- conf/db/migration/V1.002__create_service.sql
+++ conf/db/migration/V1.002__create_service.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `company_service` (
+CREATE TABLE `company_product` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(256) DEFAULT NULL,
   `detail` VARCHAR(255) DEFAULT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE `company_service` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
-INSERT INTO `company_service` (`id`, `name`, `detail`)
+INSERT INTO `company_product` (`id`, `name`, `detail`)
 VALUES
 (1,'PiedPiper',"圧縮ソフトウェア"),
 (2,'NewClear',"圧縮ソフトウェア");
